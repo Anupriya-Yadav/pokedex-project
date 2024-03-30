4108aeea84101793ab4692e2360fc5d36c6225c0
Anupriya-yadav <anupriya.1923cse1010@kiet.edu>
2024-03-30 20:54:17 +0530

    add css to pokedex

--- src/components/PokemonsDetails/PokemonDetails.jsx
+++ src/components/PokemonsDetails/PokemonDetails.jsx
@@ -1,12 +1,12 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-
+import Pokemon from "../Pokemon/Pokemon";
 function PokemonDetails() {
     const{id} = useParams();
     const [pokemon,setPokemon]=useState({});
     async function downloadPokemons(){
-        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
         
         setPokemon({
             name:response.data.name,
@@ -17,8 +17,8 @@ function PokemonDetails() {
         })
     } 
    useEffect(()=>{
-    downloadPokemons();
-   },[])
+    downloadPokemons()
+   },[]);
     return(
         <div className="pokemon-deatils-wrapper">
             <div className="pokemon-deatils-name">name:{pokemon.name}</div>
